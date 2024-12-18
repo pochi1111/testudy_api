bba42269ab3cb3dffae02e3096a9cf22ed9e414e
黒狗さん。 <kuroinusanshinji@gmail.com>
2024-12-18 23:35:49 +0900

    勉強時間追加/取得APIの作成

--- routes/times.js
+++ routes/times.js
@@ -10,19 +10,78 @@ router.get('/:uid/:amount', function (req, res, next) {
                           WHERE uid = '${req.params.uid}'
                           ORDER BY date DESC LIMIT ${req.params.amount}`, function (err, result) {
             if (err) {
-                res.json({data: [{error: "Error"}]})
+                res.status(500);
+                res.json({data: [{error: "Error : Failed to select"}]})
                 //console.error(err);
             } else {
                 res.json({data: result})
             }
         });
     } else {
+        res.status(400);
+        res.json({data: [{error: "Invalid values"}]})
+    }
+});
+
+router.post('/', function (req, res, next) {
+    if (req.body.uid && req.body.date && req.body.time) {
+        if (isNaN(req.body.time) || isNaN(Date.parse(req.body.date))) {
+            res.status(400);
+            res.json({data: [{error: "Invalid values"}]})
+            return;
+        }
+        if (req.body.time < 0) {
+            res.status(400);
+            res.json({data: [{error: "Invalid values"}]})
+            return;
+        }
+        //日時がすでにあるかを確認する
+        connection.query(`SELECT *
+                          FROM study_times
+                          WHERE uid = '${req.body.uid}' AND date = '${req.body.date}'`, function (err, result) {
+            if (err) {
+                res.status(500);
+                res.json({data: [{error: "Error : Failed to check if the date already exists"}]})
+                console.error(err);
+            } else {
+                if (result.length === 0) {
+                    connection.query(`INSERT INTO study_times (uid,date,studytime)
+                                      VALUES ('${req.body.uid}','${req.body.date}',${req.body.time})`, function (err, result) {
+                        if (err) {
+                            res.status(500);
+                            res.json({data: [{error: "Error : Failed to insert"}]})
+                            console.error(err);
+                        } else {
+                            res.status(201);
+                            res.json({data: [{success: "Success"}]})
+                        }
+                    });
+                } else {
+                    //console.log(result[0].studytime);
+                    connection.query(`UPDATE study_times
+                                      SET studytime = ${req.body.time+result[0].studytime}
+                                      WHERE uid = '${req.body.uid}' AND date = '${req.body.date}'`, function (err, result) {
+                        if (err) {
+                            res.status(500);
+                            res.json({data: [{error: "Error : Failed to update"}]})
+                            console.error(err);
+                        } else {
+                            res.status(201);
+                            res.json({data: [{success: "Success"}]})
+                        }
+                    });
+                }
+            }
+        });
+    } else {
+        res.status(400);
         res.json({data: [{error: "Invalid values"}]})
     }
 });
 
 //invalid URL
 router.all("*", (req, res) => {
+    res.status(400);
     res.json({data: [{error: "Invalid URL"}]})
 });
 
